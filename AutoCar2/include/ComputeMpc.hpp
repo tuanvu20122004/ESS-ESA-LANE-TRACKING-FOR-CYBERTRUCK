f3169d5676707efa342868e242077577986059a2
Minh Tri <khoatri26724@gmail.com>
2025-10-29 18:25:28 +0700

    Enhance comments and add derivative methods in ComputeMpc
    Updated comments for clarity and added new methods for derivatives.

--- AutoCar2/include/ComputeMpc.hpp
+++ AutoCar2/include/ComputeMpc.hpp
@@ -10,10 +10,11 @@ class ComputeMpc {
 public:
     ComputeMpc(float pixel_per_meter = 0.0002f);
     
-    // Trả về MpcState thay vì 3 tham số riêng
+    // Trả về MpcState: struct chứa cur, lateral, yaw
     MpcState computeMpcParameters(const std::vector<cv::Point>& centerline,
                                   const cv::Mat& birdEyeView);
     
+    //fit poly bậc 2 cho centerline
     cv::Vec3f fitCenterlinePoly(const std::vector<cv::Point>& centerline);
     
     std::vector<float> computeMultipleCurvatures(const cv::Vec3f& coeffs, int N = 10);
@@ -23,6 +24,7 @@ public:
     
     float computeYawAngle(const cv::Vec3f& coeffs, float y);
     
+    //Gán vị trị của xe
     void setVehiclePosition(float x, float y);
     
 private:
@@ -30,7 +32,10 @@ private:
     float vehicle_x_;
     float vehicle_y_;
     
+    //đạo hàm bậc 1
     float computeFirstDerivative(const cv::Vec3f& coeffs, float y);
+
+    //đạo hàm bậc 2
     float computeSecondDerivative(const cv::Vec3f& coeffs);
 };
 
