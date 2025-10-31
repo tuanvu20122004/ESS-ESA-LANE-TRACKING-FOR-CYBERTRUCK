8541eb931130124dcef75d20790c44152591bf5e
Minh Tri <khoatri26724@gmail.com>
2025-10-31 13:49:37 +0700

    Refactor LaneDetector class with new methods and members

--- AutoCar2/include/LaneDetector.hpp
+++ AutoCar2/include/LaneDetector.hpp
@@ -2,43 +2,58 @@
 #define LANEDETECTOR_HPP
 
 #include <opencv2/opencv.hpp>
-#include <iostream>
 #include <vector>
-#include "ComputeMpc.hpp"
-#include "MpcController.hpp"  
 
 class LaneDetector {
-private:
-    cv::VideoCapture cap;
-    int width;
-    int height;
-    ComputeMpc mpc_computer_;
-    MpcState current_state_;
-
-    float current_steering_cmd_;
-    int current_servo_angle_;
-    bool has_steering_info_;
-
 public:
     LaneDetector(const std::string& videoPath, int width = 640, int height = 480);
     ~LaneDetector();
 
     bool getFrame(cv::Mat& frame);
     bool isOpened() const;
-    
-    // Xử lý frame và cập nhật current_state_
     void processFrame(cv::Mat& frame);
     
-    // Lấy state hiện tại
-    MpcState getMpcState() const { return current_state_; }
-
-    // Thêm phương thức để set steering info
+    // Getters for MPC computation
+    std::vector<cv::Point> getCenterline() const { return centerline_; }
+    cv::Mat getBirdEyeView() const { return bird_eye_view_; }
+    bool hasValidLane() const { return has_valid_lane_; }
+    
+    // Setter for display
     void setSteeringInfo(float steering_cmd, int servo_angle) {
         current_steering_cmd_ = steering_cmd;
         current_servo_angle_ = servo_angle;
         has_steering_info_ = true;
     }
     
+    // Setter for MPC display data
+    void setMpcDisplayData(float curvature, float lateral_dev, float yaw_angle) {
+        display_curvature_ = curvature;
+        display_lateral_dev_ = lateral_dev;
+        display_yaw_angle_ = yaw_angle;
+        has_mpc_data_ = true;
+    }
+
+private:
+    cv::VideoCapture cap;
+    int width;
+    int height;
+    
+    // Display data
+    float current_steering_cmd_;
+    int current_servo_angle_;
+    bool has_steering_info_;
+    
+    float display_curvature_;
+    float display_lateral_dev_;
+    float display_yaw_angle_;
+    bool has_mpc_data_;
+    
+    // Lane detection results
+    std::vector<cv::Point> centerline_;
+    cv::Mat bird_eye_view_;
+    bool has_valid_lane_;
+    
+    // Lane detection methods
     cv::Mat applyIPM(cv::Mat& frame);
     cv::Mat processMask(const cv::Mat& bird_eye_view);
     
@@ -52,8 +67,6 @@ public:
                               cv::Mat& outImg,
                               cv::Vec3f prev_poly);
     
-    std::vector<std::vector<cv::Point>> findContoursInMask(const cv::Mat& mask);
-    
     cv::Vec3f fitPoly(const std::vector<cv::Point>& points, 
                      cv::Mat& outImg, 
                      bool isLeft);
@@ -65,6 +78,8 @@ public:
                                             cv::Mat& outImg);
     
     float computeLaneSlope(const cv::Vec3f& coeffs, float y);
+    
+    void displayInfo(cv::Mat& frame_resize, bool left_ok, bool right_ok);
 };
 
 #endif
