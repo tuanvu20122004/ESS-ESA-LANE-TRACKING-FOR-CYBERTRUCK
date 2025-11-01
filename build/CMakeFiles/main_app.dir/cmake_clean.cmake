937e5031ef4c6a302d0e2b5ade116551e051c153
lean151103-ops <lean151103@gmail.com>
2025-11-01 17:41:20 +0700

    Add LKPver1-11 project files

---
+++ build/CMakeFiles/main_app.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_app.dir/src/main.cpp.obj"
+  "CMakeFiles/main_app.dir/src/main.cpp.obj.d"
+  "libmain_app.dll.a"
+  "main_app.exe"
+  "main_app.exe.manifest"
+  "main_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
