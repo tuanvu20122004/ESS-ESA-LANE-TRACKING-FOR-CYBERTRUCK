41125991fcc950a8eafa5a3647de17402b5a73b2
Quan <Quan@>
2026-03-06 18:06:43 +0700

    [Quan] code debug lane nào đứt đoạn

--- BOSCH_22-11/src/LaneDetector.cpp
+++ BOSCH_22-11/src/LaneDetector.cpp
@@ -109,8 +109,40 @@ void LaneDetector::processFrame(cv::Mat& frame_resize) {
 
         if (left_ok)  left_coeffs  = fitPoly(left_points, bird_eye_view, true);
         if (right_ok) right_coeffs = fitPoly(right_points, bird_eye_view, false);
+        // ta có thể check lane nào là đứt ở đây
+        LaneLineType result;
 
+        if (has_left) {
+            result.left_type = classifyLaneMarking(mask, coeff_left);
+            switch (result)
+            {
+            case SOLID:
+            std::cout << "LEFTLINE: SOLID" << endl;
+                break;
+            case DASHED:
+            std::cout << "LEFTLINE: DASHED" << endl;
+                break;
+            default:
+            std::cout << "LEFTLINE: UNKNOWN" << endl;
+                break;
+            }
+        }
 
+        if (has_right) {
+            result.right_type = classifyLaneMarking(mask, coeff_right);
+            switch (result)
+            {
+            case SOLID:
+            std::cout << "RIGHTLINE: SOLID" << endl;
+                break;
+            case DASHED:
+            std::cout << "RIGHTLINE: DASHED" << endl;
+                break;
+            default:
+            std::cout << "RIGHTLINE: UNKNOWN" << endl;
+                break;
+            }
+        }
 
         if (left_ok && right_ok) {
             initialized = true;
